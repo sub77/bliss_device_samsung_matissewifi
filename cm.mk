687720317b8ebf1f942f69af7304d4936916a527
sub77 <sub77@ymail.com>
2015-04-28 11:18:54 -0400

    pac-twrp

--- cm.mk
+++ cm.mk
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 # Inherit some common Bliss stuff.
-$(call inherit-product, vendor/bliss/config/common_full_tablet_wifionly.mk)
+$(call inherit-product, vendor/cm/config/common_full_tablet_wifionly.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
