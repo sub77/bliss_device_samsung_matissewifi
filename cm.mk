aa133ad865026e23c94465bc051aba9b9b395a82
sub77 <sub77@ymail.com>
2015-05-11 20:07:03 +0200

    typo

--- cm.mk
+++ cm.mk
@@ -20,6 +20,6 @@ $(call inherit-product, device/samsung/matissewifi/full_matissewifi.mk)
 
 # Release name
 PRODUCT_RELEASE_NAME := SM-T530
-PRODUCT_NAME := bliss_matissewifi
+PRODUCT_NAME := cm_matissewifi
 
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=matissewifixx TARGET_DEVICE=matissewifi BUILD_FINGERPRINT="samsung/matissewifixx/matissewifi:5.0.2/LRX22G/T530XXU1BOD8:user/release-keys" PRIVATE_BUILD_DESC="matissewifixx-user 5.0.2 LRX22G T530XXU1BOD8 release-keys"
