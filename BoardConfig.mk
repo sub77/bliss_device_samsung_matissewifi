d4d242feaa65c6ddc55c766f9e72753317fc9478
sub77 <sub77@ymail.com>
2015-05-11 14:49:24 +0200

    merged lp5.1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -32,7 +32,7 @@ TARGET_LIBINIT_DEFINES_FILE := $(LOCAL_PATH)/init/init_matissewifi.c
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := $(LOCAL_PATH)/mkbootimg.mk
 BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 androidboot.bootdevice=msm_sdcc.1
+BOARD_KERNEL_CMDLINE := console=null androidboot.console=null androidboot.hardware=qcom user_debug=23 msm_rtb.filter=0x37 maxcpus=2 androidboot.bootdevice=msm_sdcc.1
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_SEPARATED_DT := true
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000 --tags_offset 0x1e00000
@@ -44,22 +44,42 @@ TARGET_KERNEL_VARIANT_CONFIG := msm8226-sec_matissewifi_defconfig
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 4096
 BOARD_BOOTIMAGE_PARTITION_SIZE := 0x00A00000
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01A7DEA0
+BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x00A7DEA0
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 2097152000
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 12562627584
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/twrp.fstab
-TW_THEME := landscape_hdpi
-TW_TARGET_USES_QCOM_BSP := true
-BOARD_SUPPRESS_SECURE_ERASE := true
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
 
 # SELinux
 -include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += device/samsung/matissewifi/sepolicy
+
+BOARD_SEPOLICY_DIRS += \
+       device/samsung/matissewifi/sepolicy
 
 BOARD_SEPOLICY_UNION += \
-       hostapd.te \
-       platform_app.te
+       init.te \
+       init_shell.te \
+       mm-qcamerad.te \
+       netd.te \
+       platform_app.te \
+       rmt_storage.te \
+       shell.te \
+       sysinit.te \
+       system_app.te \
+       system_server.te \
+       tee.te \
+       time_daemon.te \
+       untrusted_app.te \
+       vold.te \
+       wcnss_service.te \
+       zygote.te 	
 
-
+# Enable dex-preoptimization to speed up first boot sequence
+ifeq ($(HOST_OS),linux)
+  ifeq ($(TARGET_BUILD_VARIANT),user)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+    endif
+  endif
+endif
