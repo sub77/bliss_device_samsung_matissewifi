5e13ec69da5dd86d0218b5f18382ae19a2e2b1f7
sub77 <sub77@ymail.com>
2015-05-16 01:40:25 +0200

    enable dexpreopt for userdebug build

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,7 +77,7 @@ BOARD_SEPOLICY_UNION += \
 
 # Enable dex-preoptimization to speed up first boot sequence
 ifeq ($(HOST_OS),linux)
-  ifeq ($(TARGET_BUILD_VARIANT),user)
+  ifeq ($(TARGET_BUILD_VARIANT),userdebug)
     ifeq ($(WITH_DEXPREOPT),)
       WITH_DEXPREOPT := true
     endif
