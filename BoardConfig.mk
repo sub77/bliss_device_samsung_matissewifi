ae80ea612cd0a36f3863f0660e520cf4606edd19
sub77-bkp <sub77@ymail.com>
2015-05-19 23:36:04 +0200

    cosmetic

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,62 +13,60 @@
 # limitations under the License.
 
 
-TARGET_OTA_ASSERT_DEVICE := matissewifi,matissewifixx
+TARGET_OTA_ASSERT_DEVICE 			:= matissewifi,matissewifixx
 
 # Architecture
-TARGET_ARCH := arm
-TARGET_CPU_ABI := armeabi-v7a
-TARGET_CPU_ABI2 := armeabi
-TARGET_ARCH_VARIANT := armv7-a-neon
-TARGET_CPU_VARIANT := krait
+TARGET_ARCH 						:= arm
+TARGET_CPU_ABI 						:= armeabi-v7a
+TARGET_CPU_ABI2 					:= armeabi
+TARGET_ARCH_VARIANT 				:= armv7-a-neon
+TARGET_CPU_VARIANT 					:= krait
 
-#TARGET_LIBINIT_DEFINES_FILE := device/samsung/matissewifi/init_matissewifi.c
+BOARD_CUSTOM_BOOTIMG_MK 			:= device/samsung/matissewifi/custombootimg.mk
+TARGET_PREBUILT_KERNEL 				:= device/samsung/matissewifi/kernel
 
-BOARD_CUSTOM_BOOTIMG_MK := device/samsung/matissewifi/custombootimg.mk
-TARGET_PREBUILT_KERNEL := device/samsung/matissewifi/kernel
+TARGET_RECOVERY_FSTAB 				:= device/samsung/matissewifi/fstab.qcom
+BOARD_USE_CUSTOM_RECOVERY_FONT 		:= \"roboto_15x24.h\"
+BOARD_SUPPRESS_SECURE_ERASE 		:= true
+RECOVERY_GRAPHICS_USE_LINELENGTH 	:= true
+RECOVERY_SDCARD_ON_DATA 			:= true
+TARGET_RECOVERY_QCOM_RTC_FIX		:= true
+HAVE_SELINUX 						:= true
+TARGET_RECOVERY_PIXEL_FORMAT 		:= "RGBX_8888"
 
-TARGET_RECOVERY_FSTAB := device/samsung/matissewifi/fstab.qcom
-BOARD_USE_CUSTOM_RECOVERY_FONT := \"roboto_15x24.h\"
-BOARD_SUPPRESS_SECURE_ERASE := true
-RECOVERY_GRAPHICS_USE_LINELENGTH := true
-RECOVERY_SDCARD_ON_DATA := true
-TARGET_RECOVERY_QCOM_RTC_FIX := true
-HAVE_SELINUX := true
-TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+TW_THEME 							:= landscape_xdpi
+TW_NEW_ION_HEAP 					:= true
+TW_BRIGHTNESS_PATH 					:= /sys/class/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS 					:= 128
+TW_DEFAULT_EXTERNAL_STORAGE 		:= true
+TW_FLASH_FROM_STORAGE 				:= true
+TW_INTERNAL_STORAGE_PATH 			:= "/data/media/0"
+TW_INTERNAL_STORAGE_MOUNT_POINT 	:= "data"
+TW_EXTERNAL_STORAGE_PATH 			:= "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT 	:= "external_sd"
+TW_NO_REBOOT_BOOTLOADER 			:= true
+TW_HAS_DOWNLOAD_MODE 				:= true
+TW_NO_USB_STORAGE 					:= true
 
-TW_THEME := landscape_hdpi
-TW_NEW_ION_HEAP := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
-TW_MAX_BRIGHTNESS := 128
-TW_DEFAULT_EXTERNAL_STORAGE := true
-TW_FLASH_FROM_STORAGE := true
-TW_INTERNAL_STORAGE_PATH := "/data/media/0"
-TW_INTERNAL_STORAGE_MOUNT_POINT := "data"
-TW_EXTERNAL_STORAGE_PATH := "/external_sd"
-TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
-TW_NO_REBOOT_BOOTLOADER := true
-TW_HAS_DOWNLOAD_MODE := true
-TW_NO_USB_STORAGE := true
-TW_INCLUDE_L_CRYPTO := true
-TW_INCLUDE_CRYPTO_SAMSUNG := true
-TW_CRYPTO_FS_TYPE := "ext4"
-TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p26"
-TW_CRYPTO_MNT_POINT := "/data"
-TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,journal_async_commit,errors=panic"
-TW_CRYPTO_FS_FLAGS := "0x00000406"
-TW_CRYPTO_KEY_LOC := "footer"
+#TW_INCLUDE_L_CRYPTO := true
+#TW_INCLUDE_CRYPTO_SAMSUNG := true
+#TW_CRYPTO_FS_TYPE := "ext4"
+#TW_CRYPTO_REAL_BLKDEV := "/dev/block/mmcblk0p26"
+#TW_CRYPTO_MNT_POINT := "/data"
+#TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,noatime,noauto_da_alloc,journal_async_commit,errors=panic"
+#TW_CRYPTO_FS_FLAGS := "0x00000406"
+#TW_CRYPTO_KEY_LOC := "footer"
 
-MR_INPUT_TYPE := type_b
-MR_INIT_DEVICES := device/samsung/matissewifi/mr_init_devices.c
-MR_DPI := hdpi
-MR_DPI_FONT := 160
-MR_FSTAB := device/samsung/matissewifi/twrp.fstab
+#MR_INPUT_TYPE := type_b
+#MR_INIT_DEVICES := device/samsung/matissewifi/mr_init_devices.c
+#MR_DPI := hdpi
+#MR_DPI_FONT := 160
+#MR_FSTAB := device/samsung/matissewifi/twrp.fstab
 # End of first RAM region is 0x083fffff, so we set it to for example 0x06500000
-MR_KEXEC_MEM_MIN := 0x06500000
-MR_KEXEC_DTB := true
-MR_USE_QCOM_OVERLAY := true
-MR_QCOM_OVERLAY_HEADER := device/samsung/matissewifi/msm_mdp.h
-MR_QCOM_OVERLAY_HEADER2 := hardware/qcom/msm8x26/kernel-headers/linux/msm_ion.h
-MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
+#MR_KEXEC_MEM_MIN := 0x06500000
+#MR_KEXEC_DTB := true
+#MR_USE_QCOM_OVERLAY := true
+#MR_QCOM_OVERLAY_HEADER := device/samsung/matissewifi/msm_mdp.h
+#MR_QCOM_OVERLAY_HEADER2 := hardware/qcom/msm8x26/kernel-headers/linux/msm_ion.h
+#MR_QCOM_OVERLAY_CUSTOM_PIXEL_FORMAT := MDP_RGBX_8888
 
-#-include device/qcom/sepolicy/sepolicy.mk
